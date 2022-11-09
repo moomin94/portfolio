866093e631129a4eb6464d99a17ddbf1744e9980
moomin94 <104401041+moomin94@users.noreply.github.com>
2022-11-09 12:12:40 +0900

    Update cursor.js

--- js/cursor.js
+++ js/cursor.js
@@ -5,32 +5,32 @@ let circle;
 let x = 0, y = 0;
 let mx = 0, my = 0;
 
-window.onload = function(){
+document.addEventListener("DOMContentLoaded", function () {
     btn = document.querySelectorAll('.btn');
     cursorItem = document.querySelector(".cursorItem");
     circle = cursorItem.querySelector(".circle");
 
-    btn.forEach(function(item){
-        item.addEventListener("mouseover", function(e){
+    btn.forEach(function (item) {
+        item.addEventListener("mouseover", function (e) {
             circle.style.transform = "scale(.3)";
         });
-        item.addEventListener("mouseout", function(e){
+        item.addEventListener("mouseout", function (e) {
             circle.style.transform = "scale(1)";
         });
     });
 
-    window.addEventListener("mousemove", function(e){
+    window.addEventListener("mousemove", function (e) {
         x = e.clientX;
         y = e.clientY;
     });
-    
-    loop();
-}
 
-function loop(){
-    mx += (x - mx ) * .09;
-    my += (y - my ) * .09; 
-    cursorItem.style.transform = "translate("+ mx +"px, "+ my + "px )";
+    loop();
+});
+
+function loop() {
+    mx += (x - mx) * .09;
+    my += (y - my) * .09;
+    cursorItem.style.transform = "translate(" + mx + "px, " + my + "px )";
 
     requestAnimationFrame(loop);
 }
