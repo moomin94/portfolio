f9a67fed8a22b4e9c82463f0bd6ec1e9979fbf92
moomin94 <aktmd2529@gmail.com>
2022-11-21 16:11:56 +0900

    Remove gnb btn

--- js/common.js
+++ js/common.js
@@ -8,34 +8,6 @@ $(function(){
 		}, 300*i);
 	};
 
-	// // gnb button click
-	// $(".gnb-nav .btn a").click(function(e){
-	// 	e.preventDefault();
-	// 	let wHeight = $(window).height();
-	// 	var thisElem = $(this).attr('data-id');
-	// 	console.log(thisElem);
-	// 	$('.mySwiper > .swiper-wrapper').css({'transform': `translate3d(0px, -${wHeight*thisElem}px, 0px)`, 'transition-duration': '800ms'});
-	// 	$('.mySwiper > .swiper-wrapper > .swiper-slide').each(function(){
-	// 		$(this).removeClass('swiper-slide-active');
-	// 		$(this).removeClass('swiper-slide-prev');
-	// 		$(this).removeClass('swiper-slide-next');
-	// 	});
-	// 	$('.mySwiper > .swiper-wrapper > .swiper-slide').eq(thisElem).addClass('swiper-slide-active');
-	// 	$('.mySwiper > .swiper-wrapper > .swiper-slide').eq(thisElem).prev().addClass('swiper-slide-prev');
-	// 	$('.mySwiper > .swiper-wrapper > .swiper-slide').eq(thisElem).next().addClass('swiper-slide-next');
-	// 	$('.swiper-pagination-vertical > .swiper-pagination-bullet').each(function(){
-	// 		$(this).removeClass('swiper-pagination-bullet-active');
-	// 		$(this).attr('aria-current', '');
-	// 	});
-	// 	$('.swiper-pagination-vertical > .swiper-pagination-bullet').eq(thisElem).addClass('swiper-pagination-bullet-active').attr('aria-current', 'true');;
-	// 	// let wHeight = $(window).height();
-	// 	// if($('.mySwiper .swiper-slide').attr('id')=='home'){
-	// 	// 	console.log(thisElem.offset().top);
-	// 		// $('body').animate({scrollTop: wHeight},1500);
-	// 	// };
-  //   return false;
-  // });
-
 	// swiper button add class
 	$('.swiper-pagination-bullet').addClass('btn');
 	$('.swiper-pagination').hover(function(){
@@ -43,16 +15,6 @@ $(function(){
 	}, function(){
 		$('.cursorItem').css({'opacity': '1', 'visibility': 'visible'});
 	});
-
-	// myskill element flip
-	// for(let i = 1; i < 9; i++){
-	// 	setTimeout(function(){
-	// 		$(`.cont-wrap ul li:nth-child(${i})`).addClass('on');
-	// 	},50*i);
-	// 	setTimeout(function(){
-	// 		$(`.cont-wrap ul li:nth-child(${i})`).addClass('active');
-	// 	},100*i);
-	// };
 
 	function flipLi(){
 		if($('#myskill').hasClass('swiper-slide-active')){
