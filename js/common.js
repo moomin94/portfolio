d2c0010c2429fc0b9c19021309610b72053d228e
tmd012 <tmd012@naver.com>
2023-06-07 17:25:16 +0900

    edit swiper

--- js/common.js
+++ js/common.js
@@ -1,55 +1,55 @@
 'use strict';
 
-$(function(){
+$(function () {
 	// home text fadeIn
-	for(let i = 0; i < 2; i++){
-		setTimeout(function(){
+	for (let i = 0; i < 2; i++) {
+		setTimeout(function () {
 			$('.home.title p').eq(i).addClass('on');
-		}, 300*i);
+		}, 300 * i);
 	};
 
 	// swiper button add class
 	$('.swiper-pagination-bullet').addClass('btn');
-	$('.swiper-pagination').hover(function(){
-		$('.cursorItem').css({'opacity': '0', 'visibility': 'none'});
-	}, function(){
-		$('.cursorItem').css({'opacity': '1', 'visibility': 'visible'});
+	$('.swiper-pagination').hover(function () {
+		$('.cursorItem').css({ 'opacity': '0', 'visibility': 'none' });
+	}, function () {
+		$('.cursorItem').css({ 'opacity': '1', 'visibility': 'visible' });
 	});
 
-	function flipLi(){
-		if($('#myskill').hasClass('swiper-slide-active')){
-			for(let i = 1; i < 9; i++){
-				setTimeout(function(){
+	function flipLi() {
+		if ($('#myskill').hasClass('swiper-slide-active')) {
+			for (let i = 1; i < 9; i++) {
+				setTimeout(function () {
 					$(`.cont-wrap ul li:nth-child(${i})`).addClass('on');
-				},50*i);
-				setTimeout(function(){
+				}, 50 * i);
+				setTimeout(function () {
 					$(`.cont-wrap ul li:nth-child(${i})`).addClass('active');
-				},100*i);
+				}, 100 * i);
 			}
-		}else{
-			for(let i = 8; i > 0; i--){
-				setTimeout(function(){
+		} else {
+			for (let i = 8; i > 0; i--) {
+				setTimeout(function () {
 					$(`.cont-wrap ul li:nth-child(${i})`).removeClass('on');
-				},50*i);
-				setTimeout(function(){
+				}, 50 * i);
+				setTimeout(function () {
 					$(`.cont-wrap ul li:nth-child(${i})`).removeClass('active');
-				},100*i);
+				}, 100 * i);
 			}
 		};
 	};
-	
 
-	$('.swiper-pagination-bullet, .gnb-nav .btn a').on('click',function (){
-		for(let i = 1; i < 9; i++){
-			setTimeout(function(){
+
+	$('.swiper-pagination-bullet, .gnb-nav .btn a').on('click', function () {
+		for (let i = 1; i < 9; i++) {
+			setTimeout(function () {
 				$(`.cont-wrap ul li:nth-child(${i})`).addClass('on');
-			},50*i);
-			setTimeout(function(){
+			}, 50 * i);
+			setTimeout(function () {
 				$(`.cont-wrap ul li:nth-child(${i})`).addClass('active');
-			},100*i);
+			}, 100 * i);
 		}
 	});
-	
+
 	$(window).on('mousewheel', flipLi);
 
 });
